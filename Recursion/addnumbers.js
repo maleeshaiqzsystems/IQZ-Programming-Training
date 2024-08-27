2b69cd74193e1c01fd8cc2bd300a92b19a898d37
Maleesha Madhuhansani <maleesha.m@iqzsystems.com>
2024-08-27 13:27:01 +0530

    recursion2

---
+++ Recursion/addnumbers.js
@@ -0,0 +1,16 @@
+function getSum(number,sum){
+
+    if(number.length===0){
+        return sum;
+    }
+
+    sum+=Number(number[0]);
+    
+    return getSum(number.slice(1),sum);
+
+
+    return sum;
+}
+
+let sum =0;
+console.log(getSum("234567",sum));
